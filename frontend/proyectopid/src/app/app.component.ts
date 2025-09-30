76cdac1a21855fcfd80baafd48890a2ca0c9f62f
Jhonnfraf <147097991+Jhonnfraf@users.noreply.github.com>
2025-09-29 21:19:25 -0500

    "Agregando componentes prueba"

--- frontend/proyectopid/src/app/app.component.ts
+++ frontend/proyectopid/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+/*import { RouterOutlet } from '@angular/router';*/
+import { ApiService, CpuResp, MemResp } from './services/api.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule /*, RouterOutlet*/],
+  standalone: true
 })
-export class AppComponent {
-  title = 'proyectopid';
+
+
+export class AppComponent implements OnInit {
+  cpu: number | null = null;
+  memory: MemResp | null = null;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(){
+      this.api.getCpu().subscribe((res : CpuResp) => {
+        this.cpu = res.cpu_percent;
+      });
+      this.api.getMemory().subscribe((res : MemResp) => {
+        this.memory = res;
+      });
+  }
 }
--- frontend/proyectopid/src/app/services/api.service.ts
+++ frontend/proyectopid/src/app/services/api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface CpuResp {cpu_percent: number;}
+export interface MemResp { total: number;
+  available: number;
+  percent: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = 'http://localhost:8000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCpu(): Observable<CpuResp> {
+    return this.http.get<CpuResp>(`${this.apiUrl}/cpu`);
+  }
+
+  getMemory(): Observable<MemResp> {
+    return this.http.get<MemResp>(`${this.apiUrl}/mem`);
+  }
 }
